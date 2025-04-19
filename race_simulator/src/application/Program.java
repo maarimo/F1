e43ef7b3f279bad2c8334f0d40f4fb698eb0d074
Matheus Amorim <165201036+maarimo@users.noreply.github.com>
2025-04-19 19:30:13 -0300

    Updated
    - Classe Driver
    - Enums Scuderia
    - Logica de inscrição de drivers

--- race_simulator/src/application/Program.java
+++ race_simulator/src/application/Program.java
@@ -1,10 +1,33 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.entities.Driver;
+import model.entities.enums.Scuderia;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		List<Driver> driver = new ArrayList<>();
+		
+		for(int i=1; i<=2; i++) {
+			System.out.print("#" +i+ " Nome: ");
+			
+			String name = sc.nextLine();
+			System.out.print("Scuderia: ");
+			Scuderia scuderia = Scuderia.valueOf(sc.nextLine().toUpperCase());
+			driver.add(new Driver(name, scuderia));
+			
+		}
+		
+		for(Driver d : driver) {
+			System.out.println(d);
+		}
+		
+		sc.close();
 	}
 
 }
--- race_simulator/src/model/entities/Driver.java
+++ race_simulator/src/model/entities/Driver.java
@@ -1,5 +1,41 @@
 package model.entities;
 
-public class Driver {
+import model.entities.enums.Scuderia;
 
+public class Driver {
+	
+	private String name;
+	private Scuderia scuderia;
+	
+	public Driver() {
+	}
+
+	public Driver(String name, Scuderia scuderia) {
+		this.name = name;
+		this.scuderia = scuderia;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Scuderia getScuderia() {
+		return scuderia;
+	}
+
+	public void setScuderia(Scuderia scuderia) {
+		this.scuderia = scuderia;
+	}
+	
+	@Override
+	public String toString() {
+		return scuderia + " <---> " + name;
+	}
+	
+	
+	
 }
